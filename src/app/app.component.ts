f7ee1facc0aebe68cefb5b2be3038fe6f531c5fe
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2021-01-10 11:25:01 -0500

    added async await to fetch posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angtemplate';
+export class AppComponent implements OnInit {
+  posts: Array<any>=[]
+  baseUrl: string  = "http://localhost:3000/posts"
+
+  async getPosts(){
+    const response = await fetch (this.baseUrl)
+    const data = await response.json()
+    this.posts = await data
+  }
+
+ngOnInit(){
+  this.getPosts()
+}
 }
