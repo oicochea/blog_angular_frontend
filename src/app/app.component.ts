c368a246a55819b522b6a0c07a7ae87ee47898d9
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2021-01-11 20:41:30 -0500

    added post function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   posts: Array<any>=[]
   baseUrl: string  = "http://localhost:3000/posts"
+  
+//Binding Properties
+  createTitle: string = ""
+  createBody: string = ""
+  createDate: string = ""
+  createImg: string = ""
 
   async getPosts(){
     const response = await fetch (this.baseUrl)
     const data = await response.json()
     this.posts = await data
+  }
+
+  async createPosts(){
+    console.log(this.createTitle, this.createBody, this.createDate, this.createImg)
+    await fetch(this.baseUrl,{
+      method:"post",
+      headers:{
+        "Content-Type" : "application/json",
+      },
+      body:JSON.stringify({
+        title: this.createTitle,
+        body: this.createBody,
+        date: this.createDate,
+        img: this.createImg
+      }),
+    })
+    this.getPosts()
+    this.createTitle =" "
+    this.createBody = " "
+    this.createDate = " "
+    this.createImg = " "
   }
 
 ngOnInit(){
